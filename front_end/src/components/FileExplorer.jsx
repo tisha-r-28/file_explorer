fe2c890eaf41300fa9d28af9e7e564844ecf8176
tisha-r-28 <tisha298.rejoice@gmail.com>
2024-08-21 10:00:57 +0530

    basic components done | FileExplorer, Files and SIdebar | added bootstrap link in index.js | removed unecessary imported file from app.js and index.js

---
+++ front_end/src/components/FileExplorer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FileExplorer() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default FileExplorer;
---
+++ front_end/src/components/Files.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Files() {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Files;
