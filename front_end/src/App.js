04ab79af5ae93e86741cad4e1ef12229732a6254
tisha-r-28 <tisha298.rejoice@gmail.com>
2024-08-21 16:01:42 +0530

    Added Logic for toggle the file and folder |  remain things : add folder or file, input field logic

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,24 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import fileExplorerData from "./data/fileExplorerData";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ explorerData, setExplorerData ] = useState(fileExplorerData);
+    return (
+        <>
+            <Sidebar explorerData={explorerData} setExplorerData={setExplorerData}/>
+        </>
+    );
 }
 
 export default App;
